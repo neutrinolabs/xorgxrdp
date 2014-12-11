7c17cfd4e12f92b9385412fb9b7de5433138e6d4
Jay Sorg <jay.sorg@gmail.com>
2014-12-10 23:01:22 -0800

    work on uyvy / yuy2 to rgb asm files

--- module/amd64/uyvy_to_rgb32_amd64_sse2.asm
+++ module/amd64/uyvy_to_rgb32_amd64_sse2.asm
@@ -1,16 +1,119 @@
 
+SECTION .data
+align 16
+c128 times 8 dw 128
+c4669 times 8 dw 4669
+c1616 times 8 dw 1616
+c2378 times 8 dw 2378
+c9324 times 8 dw 9324
+
+SECTION .text
+
 %macro PROC 1
     align 16
     global %1
     %1:
 %endmacro
 
+;The first six integer or pointer arguments are passed in registers
+; RDI, RSI, RDX, RCX, R8, and R9
+
 ;int
 ;uyvy_to_rgb32_amd64_sse2(unsigned char *yuvs, int width, int height, int *rgbs)
 
 PROC uyvy_to_rgb32_amd64_sse2
     push rbx
+    push rbp
+
+    mov rax, rsi
+    imul rax, rdx
+    mov rsi, rdi
+    mov rdi, rcx
+
+    mov rcx, rax
+
+    movdqa xmm7, [rel c128]
+
+loop1:
+    ; hi                                           lo
+    ; y7 v3 y6 u3 y5 v2 y4 u2 y3 v1 y2 u1 y1 v0 y0 u0
+    movdqu xmm0, [rsi]      ; 8 pixels at a time
+    lea rsi, [rsi + 16]
+
+    ; 00 y7 00 y6 00 y5 00 y4 00 y3 00 y2 00 y1 00 y0
+    ; 00 u3 00 u3 00 u2 00 u2 00 u1 00 u1 00 u0 00 u0
+    ; 00 v3 00 v3 00 v2 00 v2 00 v1 00 v1 00 v0 00 v0
+
+    movdqu xmm1, xmm0
+    movdqu xmm2, xmm0
+
+    ; y
+    psrlw xmm0, 8
+
+    ; u
+    pslld xmm1, 24
+    psrld xmm1, 24
+    movdqu xmm3, xmm1
+    pslld xmm3, 16
+    por xmm1, xmm3
+    psubw xmm1, xmm7
+    psllw xmm1, 4
+
+    ; v
+    pslld xmm2, 8
+    psrld xmm2, 24
+    movdqu xmm3, xmm2
+    pslld xmm3, 16
+    por xmm2, xmm3
+    psubw xmm2, xmm7
+    psllw xmm2, 4
+
+    ; r = y + hiword(4669 * (v << 4))
+    movdqa xmm4, [rel c4669]
+    pmulhw xmm4, xmm1
+    movdqa xmm3, xmm0
+    paddw xmm3, xmm4
+
+    ; g = y - hiword(1616 * (u << 4)) - hiword(2378 * (v << 4))
+    movdqa xmm5, [rel c1616]
+    pmulhw xmm5, xmm2
+    movdqa xmm6, [rel c2378]
+    pmulhw xmm6, xmm1
+    movdqa xmm4, xmm0
+    psubw xmm4, xmm5
+    psubw xmm4, xmm6
+
+    ; b = y + hiword(9324 * (u << 4))
+    movdqa xmm6, [rel c9324]
+    pmulhw xmm6, xmm2
+    movdqa xmm5, xmm0
+    paddw xmm5, xmm6
+
+    packuswb xmm3, xmm3  ; b
+    packuswb xmm4, xmm4  ; g
+    punpcklbw xmm3, xmm4 ; gb
+
+    pxor xmm4, xmm4      ; a
+    packuswb xmm5, xmm5  ; r
+    punpcklbw xmm5, xmm4 ; ar
+
+    movdqa xmm4, xmm3
+
+    punpcklwd xmm3, xmm5 ; argb
+    movdqa [rdi], xmm3   ; 4 pixels
+    lea rdi, [rdi + 16]
+
+    punpckhwd xmm4, xmm5 ; argb
+    movdqa [rdi], xmm4   ; 4 pixels
+    lea rdi, [rdi + 16]
+
+    sub rcx, 8
+    cmp rcx, 8
+    jge loop1
+
     mov rax, 0
+
+    pop rbp
     pop rbx
     ret
     align 16
--- module/amd64/yuy2_to_rgb32_amd64_sse2.asm
+++ module/amd64/yuy2_to_rgb32_amd64_sse2.asm
@@ -1,16 +1,119 @@
 
+SECTION .data
+align 16
+c128 times 8 dw 128
+c4669 times 8 dw 4669
+c1616 times 8 dw 1616
+c2378 times 8 dw 2378
+c9324 times 8 dw 9324
+
+SECTION .text
+
 %macro PROC 1
     align 16
     global %1
     %1:
 %endmacro
 
+;The first six integer or pointer arguments are passed in registers
+; RDI, RSI, RDX, RCX, R8, and R9
+
 ;int
 ;yuy2_to_rgb32_amd64_sse2(unsigned char *yuvs, int width, int height, int *rgbs)
 
 PROC yuy2_to_rgb32_amd64_sse2
     push rbx
+    push rbp
+
+    mov rax, rsi
+    imul rax, rdx
+    mov rsi, rdi
+    mov rdi, rcx
+
+    mov rcx, rax
+
+    movdqa xmm7, [rel c128]
+
+loop1:
+    ; hi                                           lo
+    ; v7 y3 u6 y3 v5 y2 u4 y2 v3 y1 u2 y1 v1 y0 u0 y0
+    movdqu xmm0, [rsi]      ; 8 pixels at a time
+    lea rsi, [rsi + 16]
+
+    ; 00 y7 00 y6 00 y5 00 y4 00 y3 00 y2 00 y1 00 y0
+    ; 00 u3 00 u3 00 u2 00 u2 00 u1 00 u1 00 u0 00 u0
+    ; 00 v3 00 v3 00 v2 00 v2 00 v1 00 v1 00 v0 00 v0
+
+    movdqu xmm1, xmm0
+    movdqu xmm2, xmm0
+
+    ; y
+    psllw xmm0, 8
+    psrlw xmm0, 8
+
+    ; u
+    pslld xmm1, 16
+    psrld xmm1, 24
+    movdqu xmm3, xmm1
+    pslld xmm3, 16
+    por xmm1, xmm3
+    psubw xmm1, xmm7
+    psllw xmm1, 4
+
+    ; v
+    psrld xmm2, 24
+    movdqu xmm3, xmm2
+    pslld xmm3, 16
+    por xmm2, xmm3
+    psubw xmm2, xmm7
+    psllw xmm2, 4
+
+    ; r = y + hiword(4669 * (v << 4))
+    movdqa xmm4, [rel c4669]
+    pmulhw xmm4, xmm1
+    movdqa xmm3, xmm0
+    paddw xmm3, xmm4
+
+    ; g = y - hiword(1616 * (u << 4)) - hiword(2378 * (v << 4))
+    movdqa xmm5, [rel c1616]
+    pmulhw xmm5, xmm2
+    movdqa xmm6, [rel c2378]
+    pmulhw xmm6, xmm1
+    movdqa xmm4, xmm0
+    psubw xmm4, xmm5
+    psubw xmm4, xmm6
+
+    ; b = y + hiword(9324 * (u << 4))
+    movdqa xmm6, [rel c9324]
+    pmulhw xmm6, xmm2
+    movdqa xmm5, xmm0
+    paddw xmm5, xmm6
+
+    packuswb xmm3, xmm3  ; b
+    packuswb xmm4, xmm4  ; g
+    punpcklbw xmm3, xmm4 ; gb
+
+    pxor xmm4, xmm4      ; a
+    packuswb xmm5, xmm5  ; r
+    punpcklbw xmm5, xmm4 ; ar
+
+    movdqa xmm4, xmm3
+
+    punpcklwd xmm3, xmm5 ; argb
+    movdqa [rdi], xmm3   ; 4 pixels
+    lea rdi, [rdi + 16]
+
+    punpckhwd xmm4, xmm5 ; argb
+    movdqa [rdi], xmm4   ; 4 pixels
+    lea rdi, [rdi + 16]
+
+    sub rcx, 8
+    cmp rcx, 8
+    jge loop1
+
     mov rax, 0
+
+    pop rbp
     pop rbx
     ret
     align 16
