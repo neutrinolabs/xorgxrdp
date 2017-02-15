a31604c7ec5149322d8272f21f6d7096aeede2fe
Jay Sorg <jay.sorg@gmail.com>
2017-02-15 01:45:55 -0800

    simplify capture, remove unnecessary parameters

--- module/rdpCapture.h
+++ module/rdpCapture.h
@@ -33,13 +33,8 @@ capture
 #define MAX_CAPTURE_RECTS 15
 
 extern _X_EXPORT Bool
-rdpCapture(rdpClientCon *clientCon,
-           RegionPtr in_reg, BoxPtr *out_rects, int *num_out_rects,
-           const char *src, int src_left, int src_top,
-           int src_width, int src_height,
-           int src_stride, int src_format,
-           char *dst, int dst_width, int dst_height,
-           int dst_stride, int dst_format, int mode);
+rdpCapture(rdpClientCon *clientCon, RegionPtr in_reg, BoxPtr *out_rects,
+           int *num_out_rects, struct image_data *id);
 
 extern _X_EXPORT int
 a8r8g8b8_to_a8b8g8r8_box(const char *s8, int src_stride,
