86a66cdc0dfdfaed27ff00869995f747af50e44c
matt335672 <30179339+matt335672@users.noreply.github.com>
2021-02-17 14:00:26 +0000

    Move to github actions for CI (#182)

---
+++ scripts/install_xorgxrdp_build_dependencies_with_apt.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+set -eufx
+
+if [ -z "${1:-}" ]; then
+    # No arch specified
+    ARCH=amd64
+elif [ "${1#--}" != "$1" ]; then
+    # First parameter starts with '--'
+    ARCH=amd64
+else
+    ARCH=$1
+    shift
+fi
+
+APT_EXTRA_ARGS="$@"
+
+# common build tools for all architectures and feature sets
+PACKAGES=" \
+    xserver-xorg-core \
+    xserver-xorg-dev \
+    nasm \
+    "
+
+case "$ARCH"
+in
+    amd64)
+        ;;
+    i386)
+        PACKAGES="$PACKAGES \
+            gcc-multilib \
+            "
+        dpkg --add-architecture i386
+        dpkg --print-architecture
+        dpkg --print-foreign-architectures
+        apt-get update
+        ;;
+    *)
+        echo "unsupported architecture: $ARCH"
+        exit 1;
+        ;;
+esac
+
+exec apt-get -yq \
+    --no-install-suggests \
+    --no-install-recommends \
+    $APT_EXTRA_ARGS \
+    install $PACKAGES
