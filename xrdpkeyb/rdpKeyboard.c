e1c82b4b08a7a48583d28cfa63ac63d3d565a8e2
Koichiro Iwao <meta@vmeta.jp>
2026-01-29 20:40:13 +0900

    Fix build with xlibre
    The display number moved to the DIX layer, no longer available in this
    scope. Just removing it from the log as it is not necessary.
    
    References:
    - https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1783
    - https://github.com/neutrinolabs/xorgxrdp/pull/364

--- xrdpkeyb/rdpKeyboard.c
+++ xrdpkeyb/rdpKeyboard.c
@@ -863,8 +863,8 @@ rdpLoadLayout(rdpKeyboard *keyboard, struct xrdp_client_info *client_info)
 
     int keylayout = client_info->keylayout;
 
-    LLOGLN(0, ("rdpLoadLayout: keylayout 0x%8.8x variant %s display %s",
-               keylayout, client_info->variant, display));
+    LLOGLN(0, ("rdpLoadLayout: keylayout 0x%8.8x variant %s",
+               keylayout, client_info->variant));
     memset(&set, 0, sizeof(set));
     set.rules = g_base_str;
 
