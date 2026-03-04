d45411a80483c0378cfe900fb5381bbbd38643ab
matt335672 <30179339+matt335672@users.noreply.github.com>
2026-03-04 11:31:23 +0000

    Code quality: Address more Copilot issues
    - xrdpdev/xrdpdev.c
      - Wrong string terminated
    
    - module/rdpClientCon.c
      - A failure of g_alloc_shm_map_fd() should be treated as fatal,
        as we're not set up to handle it.
    
    (cherry picked from commit 13b31ff9fa27433994e4473a8180c0529a20a8ad)

--- xrdpdev/xrdpdev.c
+++ xrdpdev/xrdpdev.c
@@ -983,7 +983,7 @@ rdpProbe(DriverPtr drv, int flags)
         {
 #if defined(XORGXRDP_GLAMOR)
             strncpy(g_drm_allow_list, val, 127);
-            g_drm_device[127] = 0;
+            g_drm_allow_list[127] = 0;
             LLOGLN(0, ("rdpProbe: found DRMAllowList xorg.conf value [%s]", val));
 #endif
         }
